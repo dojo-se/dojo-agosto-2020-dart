1f88cde95a21776222bc598c655a5126baf22d57
LUCAS SOARES DO REGO <lucas.sr.rg@gmail.com>
2020-08-20 00:17:17 +0000

    removido o if e acrescentado teste para mais coluna

--- main.dart
+++ main.dart
@@ -25,18 +25,29 @@
 // list[linha][coluna] <--
 
 List<List<int>> espiral(int colunas, int linhas) {
-	if(linhas == 1 && colunas == 1){
-    return [[1]];
-  }
-  else{
+	
     var list = List<List<int>>.generate(linhas, (int index) => List<int>.generate(colunas, (int index) => 0));
 
-    for(var i=0;i < colunas; i++ ){
-      list[i][0];
+    
+
+    // Para direita
+    for(var i = 0; i< colunas; i++ ){
+      list[0][i] = i+1;
     }
-    print(list)
+
+    // Para baixo 
+   
+    
+    // Para esquerda
+
+
+
+    // Para cima 
+    
+    
+
     return list;
-  }
+  
 }
 
 
@@ -48,6 +59,14 @@ void main() {
 
   test('Entrada com (2 colunas e 1 linha)', () {
     expect(espiral(2, 1), equals([[1,2]]));
+  });
+
+   test('Entrada com (4 colunas e 1 linha)', () {
+    expect(espiral(4, 1), equals([[1,2,3,4]]));
+  });
+
+  test('Entrada com (1 coluna e 2 linhas)', () {
+    expect(espiral(1, 2), equals([[1],[2]]));
   });
 
   
